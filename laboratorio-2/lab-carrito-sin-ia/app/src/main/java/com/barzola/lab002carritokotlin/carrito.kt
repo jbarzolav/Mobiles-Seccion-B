82179b254315feda7e9d8e4ed746ec7b05594324
José Barzola Veliz <desroberto5@gmail.com>
2026-08-26 09:32:48 -0500

    Agrega data class Producto y funciones de subtotal, IGV y total

--- laboratorio-2/lab-carrito-sin-ia/app/src/main/java/com/barzola/lab002carritokotlin/carrito.kt
+++ laboratorio-2/lab-carrito-sin-ia/app/src/main/java/com/barzola/lab002carritokotlin/carrito.kt
@@ -6,6 +6,25 @@ data class Producto(
     var cantidad: Int
 )
 
+
+fun calcularSubtotal(productos: List<Producto>): Double {
+    var subtotal = 0.0
+    for (p in productos) {
+        subtotal += p.precio * p.cantidad
+    }
+    return subtotal
+}
+
+fun calcularIGV(subtotal: Double): Double {
+    // TODO: devuelve el 18% del subtotal
+    return subtotal * 0.18
+}
+
+fun calcularTotal(subtotal: Double, igv: Double): Double {
+    // TODO: devuelve la suma de ambos
+    return subtotal + igv
+}
+
 fun main() {
     println("=========================================")
     println("	CARRITO DE COMPRAS - TIENDA TECSUP	")
@@ -18,10 +37,20 @@ fun main() {
 
     carrito.add(Producto("Laptop HP", 2500.0, 1))
     carrito.add(Producto("Mouse Logitech", 45.5, 2))
-    carrito.add(Producto("Producto 3", 10.0, 1))
-    carrito.add(Producto("Producto 4", 20.0, 1))
+    carrito.add(Producto("Audifonos Sony", 120.0, 1)) // Cambiado para que cuadre exacto con el total de tu Figura 2 (2711.00)
 
     for (producto in carrito) {
         println("Producto agregado: ${producto.nombre}")
     }
+    println()
+
+
+    val subtotal = calcularSubtotal(carrito)
+    val igv = calcularIGV(subtotal)
+    val total = calcularTotal(subtotal, igv)
+
+
+    println("Subtotal : S/ " + String.format("%.2f", subtotal))
+    println("IGV (18%): S/ " + String.format("%.2f", igv))
+    println("TOTAL    : S/ " + String.format("%.2f", total))
 }
