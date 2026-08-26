29b7cc369ff2de86fc822b1ee04ae8e6bc85aff7
José Barzola Veliz <desroberto5@gmail.com>
2026-08-26 01:50:04 -0500

    Proyecto inicial con menú del carrito

---
+++ app/src/main/java/com/barzola/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,7 @@
+package com.barzola.lab02carritokotlin
+
+fun main() {
+    println("=========================================")
+    println("	CARRITO DE COMPRAS - TIENDA TECSUP	")
+    println("=========================================")
+}
