159e0e0662b888fe6ac73d7fe4a9a700e3caed98
José Barzola Veliz <desroberto5@gmail.com>
2026-08-26 08:30:10 -0500

    Modelo de datos y variables

--- laboratorio-2/lab-carrito-sin-ia/app/src/main/java/com/barzola/lab002carritokotlin/carrito.kt
+++ laboratorio-2/lab-carrito-sin-ia/app/src/main/java/com/barzola/lab002carritokotlin/carrito.kt
@@ -1,7 +1,27 @@
-package com.barzola.lab002carritokotlin
+package com.barzola.lab02carritokotlin
+
+data class Producto(
+    val nombre: String,
+    val precio: Double,
+    var cantidad: Int
+)
 
 fun main() {
     println("=========================================")
     println("	CARRITO DE COMPRAS - TIENDA TECSUP	")
     println("=========================================")
+
+    val nombreCliente = "Juan Leon"
+    val carrito = mutableListOf<Producto>()
+    println("Cliente: $nombreCliente")
+    println()
+
+    carrito.add(Producto("Laptop HP", 2500.0, 1))
+    carrito.add(Producto("Mouse Logitech", 45.5, 2))
+    carrito.add(Producto("Producto 3", 10.0, 1))
+    carrito.add(Producto("Producto 4", 20.0, 1))
+
+    for (producto in carrito) {
+        println("Producto agregado: ${producto.nombre}")
+    }
 }
