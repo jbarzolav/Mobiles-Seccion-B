b2be047dc3362b7d08e9261e407e72c8692d7430
José Barzola Veliz <desroberto5@gmail.com>
2026-08-27 18:00:00 -0500

    Parte 3: Tabla de multas con String.format

--- semana 3/prestamo libros tarea/app/src/main/java/com/barzola/prestamodelibros/prestamo.kt
+++ semana 3/prestamo libros tarea/app/src/main/java/com/barzola/prestamodelibros/prestamo.kt
@@ -13,3 +13,22 @@ fun calcularMulta(diasAtraso: Int): Double {
     val MULTA_POR_DIA = 1.50
     return diasAtraso * MULTA_POR_DIA
 }
+
+fun mostrarTablaMultas(diasAtraso: Int): String {
+    val sb = StringBuilder()
+    val MULTA_POR_DIA = 1.50
+
+    sb.appendLine()
+    sb.appendLine(String.format("%-5s %-12s %-12s %-12s", "Dia", "Fecha", "Multa/Dia", "Acumulado"))
+    sb.appendLine("------------------------------------------------")
+
+    for (i in 1..diasAtraso) {
+        val acumulado = i * MULTA_POR_DIA
+        sb.appendLine(String.format("%-5d %-12s S/ %-10.2f S/ %-10.2f", i, "Dia $i", MULTA_POR_DIA, acumulado))
+    }
+
+    sb.appendLine("------------------------------------------------")
+    sb.appendLine(String.format("Multa Total: S/ %.2f", diasAtraso * MULTA_POR_DIA))
+
+    return sb.toString()
+}
