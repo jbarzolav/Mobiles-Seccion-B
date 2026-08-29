87c7c3db8f0565619506a42a9ca655c29517b289
José Barzola Veliz <desroberto5@gmail.com>
2026-08-29 11:00:00 -0500

    Eliminar modulo console y simplificar ejecucion

--- laboratorio-2/lab-carrito-con-ia/ejecutar.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title Carrito de Compras - Con IA (OOP)
-set JAVA="C:\Program Files\Eclipse Adoptium\jdk-25.0.1.8-hotspot\bin\java.exe"
-set JAR=%~dp0console\build\libs\console.jar
-%JAVA% -jar "%JAR%"
-pause
