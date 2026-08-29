cfe34af287df5c9ae37eca809427be49acec0566
José Barzola Veliz <desroberto5@gmail.com>
2026-08-29 10:30:00 -0500

    Eliminar modulo console y simplificar ejecucion

--- semana 3/prestamo libros tarea/ejecutar.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo =========================================
-echo    SISTEMA DE MULTAS - BIBLIOTECA
-echo =========================================
-echo.
-java -jar console\build\libs\console.jar
-echo.
-pause
