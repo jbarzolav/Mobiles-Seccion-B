8d50a36717656b82f2ef9b94755926b99c098412
José Barzola Veliz <desroberto5@gmail.com>
2026-09-22 22:35:46 -0500

    Creacion de la Estructura de paquetes y Archivos

--- semana 5/laboratorio5/app/build.gradle.kts
+++ semana 5/laboratorio5/app/build.gradle.kts
@@ -51,5 +51,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
-    implementation("androidx.navigation:navigation-compose:2.7.7") 
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
